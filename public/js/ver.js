e3e16fc8b053857feddaa9d066a03239b860be3b
kehylaicaza <keilacicaza@gmail.com>
2017-09-08 19:04:21 -0500

    rayito

--- public/js/ver.js
+++ public/js/ver.js
@@ -1,7 +1,7 @@
 
 var v;
 var controladorImagenes = 0;
-function leerCuento() {
+function indiceCuentos() {
     var a = 0;
 var datos="";
 $.ajax({
@@ -67,91 +67,10 @@ alert("Pinterest Amigos"+img);
            }
 $(document).ready(function () {
      
-    leerCuento();
-   
-    var arrayCuentos = [];
-   /* $.getJSON('../php/datos.json', function (data) {
-        for (let objeto of data) {
-            for (let cuento of objeto.cuentos) {
-                let aux = new Cuento();
-                aux.constru(cuento.titulo, cuento.descripcion, cuento.credito, cuento.imagenes, cuento.audios, cuento.preguntas);
-
-                arrayCuentos.push(aux);
-
-                exportar(arrayCuentos);
-
-        } 
-        }
-*/
-        
-   /* var datos="";
-      $.ajax({
-            url: '/cargar',
-            method: 'GET',
-            
-            success: function (data) {
-                console.log(data);
-                alert(data);
-                datos=data;
-            }
-        });
-    
-    
-    alert("mpstrando"+datos);
-    alert(datos);
-    */
-    
-    
-    
-   /* 
-    
-    var datos="";
-$.ajax({
-url: '/cargar',
-type: 'GET',
-
-cache: false,
-contentType: false,
-processData: false,
-
-success: function (data) {
-
-console.log(data);
-datos=data;
-},
-//si ha ocurrido un error
-error: function () {
-console.log("error");
-
-}
-});
-
-alert("Disfruta de todos los cuentos!" + datos);
-alert(datos);
-    
-    
-    
-    
-    
-    
-    
-    
-
-        //mostrar los cuentos
-        $("#principal").empty();
-        for (let value of arrayCuentos) {
-            console.log("hola soy un cuento" + arrayCuentos);
-            if (value.imagenes.length != null) {
-                $("#principal").append('<li><a id=' + a.toString() + '><div class="col-lg-4"><img class="imagenesCuentos"  src=' + value.imagenes[0].src + '>' + value.titulo + '</div></a></li>');
-
-                a++;
-            }
-
-        }
-*/   
+    indiceCuentos();
+ 
     });
 
-/*
 
 
 
@@ -191,6 +110,7 @@ alert(datos);
         mostrarCuento(v, arrayCuentos);
     })
 
+/*
 
 
     $("#anterior").click(function () {
